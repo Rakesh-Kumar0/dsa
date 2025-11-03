0af4765907ab2cb8eee775e8934202aa878c9f9a
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2025-11-03 16:59:07 +0530

    adding new file

---
+++ linkedlist/singlylinkedlist/basicOfLL.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Node class represents node in the linked list
+
+class Node {
+    public:
+    int data; // Data value
+    Node* next; //Pointer to next node
+    
+    // constructor with data and next
+    Node(int data1, Node* next1){
+        data = data1;
+        next = next1;
+    }
+    
+    // constructor with data
+    Node(int data1){
+        data = data1; 
+        next = nullptr; 
+    }
+};
+int main(){
+    // creating an array
+    vector<int> arr = {2,5,9,6};
+
+    // creating first node
+    Node* node = new Node(arr[0]);
+
+    //printing memory address of node
+    cout <<node <<endl;
+
+    // printing data value of node
+    cout <<"the value of data in node : " <<node->data <<endl;
+    return 0;
+}
