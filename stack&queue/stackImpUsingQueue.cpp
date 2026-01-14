d1bb0c1d8e8c29f6b167b634be5d34a7f8e356b3
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-01-14 23:09:15 +0530

    adding new file

--- stack&queue/stackImpUsingQueue.cpp
+++ stack&queue/stackImpUsingQueue.cpp
@@ -6,103 +6,69 @@
 #include<algorithm>
 using namespace std;
 
-// Class implementing Queue using Arrays
-class ArrayQueue {
-    // Array to store queue elements
-    int* arr;
-    // Indices for start and end of the queue
-    int start, end;
-    // Current size and maximum size of the queue
-    int currSize, maxSize;
+
+// Stack implementation using Queue
+class QueueStack {
+    // Queue
+    queue<int> q;
 
 public:
-    // Constructor
-    ArrayQueue() {
-        arr = new int[10];
-        start = -1;
-        end = -1;
-        currSize = 0;
-        maxSize = 10;
-    }
-
-    // Method to push an element into the queue
+    // Method to push element in the stack
     void push(int x) {
-        // Check if the queue is full
-        if (currSize == maxSize) {
-            cout << "Queue is full\nExiting..." << endl;
-            exit(1);
+        // Get size 
+        int s = q.size(); 
+        // Add element
+        q.push(x); 
+
+        // Move elements before new element to back
+        for (int i = 0; i < s; i++) {
+            q.push(q.front()); 
+            q.pop(); 
         }
-        
-        // If the queue is empty, initialize start and end
-        if (end == -1) {
-            start = 0;
-            end = 0;
-        } 
-        else {
-            // Circular increment of end
-            end = (end + 1) % maxSize;
-        }
-            
-        arr[end] = x;
-        currSize++;
     }
 
-    // Method to pop an element from the queue
+    // Method to pop element from stack
     int pop() {
-        // Check if the queue is empty
-        if (start == -1) {
-            cout << "Queue Empty\nExiting..." << endl;
-            exit(1);
-        }
-        int popped = arr[start];
-        
-        // If the queue has only one element, reset start and end
-        if (currSize == 1) {
-            start = -1;
-            end = -1;
-        }
-        else {
-            // Circular increment of start
-            start = (start + 1) % maxSize;
-        }
-        
-        currSize--;
-        return popped;
+        // Get front element 
+        int n = q.front(); 
+        // Remove front element
+        q.pop(); 
+        // Return removed element
+        return n; 
     }
 
-    // Method to get the front element of the queue
-    int peek() {
-        // Check if the queue is empty
-        if (start == -1) {
-            cout << "Queue is Empty" << endl;
-            exit(1);
-        }
-        return arr[start];
+    // Method to return the top of stack
+    int top() {
+        // Return front element
+        return q.front(); 
     }
 
-    // Method to determine whether the queue is empty
+    // Method to check if the stack is empty
     bool isEmpty() {
-        return (currSize == 0);
+        return q.empty(); 
     }
 };
 
 int main() {
-    ArrayQueue queue;
-    vector<string> commands = {"ArrayQueue", "push", "push", 
-                               "peek", "pop", "isEmpty"};
-    vector<vector<int>> inputs = {{}, {5}, {10}, {}, {}, {}};
+    QueueStack st;
+    
+    // List of commands
+    vector<string> commands = {"QueueStack", "push", "push", 
+                               "pop", "top", "isEmpty"};
+    // List of inputs
+    vector<vector<int>> inputs = {{}, {4}, {8}, {}, {}, {}};
 
     for (int i = 0; i < commands.size(); ++i) {
         if (commands[i] == "push") {
-            queue.push(inputs[i][0]);
+            st.push(inputs[i][0]);
             cout << "null ";
         } else if (commands[i] == "pop") {
-            cout << queue.pop() << " ";
-        } else if (commands[i] == "peek") {
-            cout << queue.peek() << " ";
+            cout << st.pop() << " ";
+        } else if (commands[i] == "top") {
+            cout << st.top() << " ";
         } else if (commands[i] == "isEmpty") {
-            cout << (queue.isEmpty() ? "true" : "false") << " ";
-        } else if (commands[i] == "ArrayQueue") {
+            cout << (st.isEmpty() ? "true" : "false") << " ";
+        } else if (commands[i] == "QueueStack") {
             cout << "null ";
         }
     }
